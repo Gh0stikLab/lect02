4b2f65a1aa8c785fd509025841be2464eab3af5a
rnn <rubannn@gmail.com>
2022-10-18 13:15:59 +0200

    add function  get_array_names()

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import names
 def get_one_name():
     return names.get_full_name()
 
+def get_array_names(count):
+    return [names.get_full_name() for _ in range(count)]
+
 if __name__ == '__main__':
     print(f'{get_one_name()=}')
+    print(f'{get_array_names(7)=}')
